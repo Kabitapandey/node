6209537608474e33ea6160afd4bacee6a1d16eaf
Kabita Panday <55394377+Kabitapandey@users.noreply.github.com>
2022-12-18 10:22:05 +0545

    key verification

--- app.js
+++ app.js
@@ -4,41 +4,39 @@ const readline=require("readline");
 const bcryptjs=require("bcryptjs");
 const express=require("express")
 const app=express();
+const bodyParser=require("body-parser")
+
 
 let encryptionKey;
+app.use(express.json())
 
-// generationg the hashed value of encryption key
+// generating the hashed value of encryption key
 bcryptjs.genSalt(10,(err,salt)=>{
     bcryptjs.hash(process.env.KEY,salt,(err,hash)=>{
         encryptionKey=hash;
     })
 })
 
-// input from the user
-const r1=readline.createInterface(process.stdin,process.stdout)
-
-
 // server listening at port 3000
 app.listen(3000,()=>{
     console.log("Server is listening at port 3000...");
-    checkToken();
 })
 
+app.post("/",(req,res)=>{
 
+    let {key}=req.body;
 
-const checkToken=()=>{
-    r1.question("Enter the key:\n",(key)=>{
-        // comparing the ecryption key provided with the encryption key
-        bcryptjs.compare(key,encryptionKey,(err,isMatch)=>{
-            // if encryption key entered is valid than only the user can access the page
-            if(isMatch){
-                app.use(express.static("./public"))
-            }
+    console.log(key);
+    bcryptjs.compare(key,encryptionKey,(err,isMatch)=>{
+           
+        // if encryption key entered is valid than only the user can access the page
+        if(isMatch){
+            res.json({msg:"valid"})
+        }
 
-            else{
-                console.log("Invalid key");
-            }
-        })
-        r1.close()
+        else{
+            res.json({msg:"invalid"})
+        }
     })
-}
+})
+
